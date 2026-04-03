7cb88abbf71a0f0c94b78429df07a862ba0672fb
mecchumphon <meccp.ckusk@cpird.in.th>
2026-04-03 16:06:28 +0700

    Add files via upload

---
+++ Js/api-config.js
@@ -0,0 +1,5 @@
+// ============================================================
+//  ตั้งค่า Google Apps Script Web App URL
+//  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
+// ============================================================
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwcP4hjqvJqyLtbXhd4ifwusSVSB_Y7TP9uYUW6_LSKCzd920_Wv8BRWzH7FmqmHZRmBQ/exec';
